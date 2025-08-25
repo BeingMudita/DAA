4690578feb36846ce9ceb078fe40b2caa494fac5
Mudita Jain <mudita21jain@gmail.com>
2025-08-25 13:43:02 +0530

    `Added ternary search function and main test case`

--- Divide & Conquer/TernarySearch.cpp
+++ Divide & Conquer/TernarySearch.cpp
@@ -21,4 +21,16 @@ int ternary_search(int arr[], int l, int r, int key){
             return ternary_search(arr, mid1+1, mid2-1, key); //T(n/3)
         }
     }
+    return -1;
+}
+
+int main(){
+    int arr[] = {9, 10, 28, 49, 78, 90, 101};
+    int key = 50;
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int result = ternary_search(arr,0, n-1,key);
+    (result==-1)
+    ?cout<<"Element is not present in array"
+    :cout<<"Element is present at index "<<result;
+    return 0;
 }
