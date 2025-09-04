4d4426c35bb0c83837bf94872f51af57a503cd41
Mudita Jain <mudita21jain@gmail.com>
2025-09-04 10:51:22 +0530

    `Added divide and conquer matrix multiplication implementation with main function for user input and output.`

--- Divide & Conquer/MatrixMultiplication.cpp
+++ Divide & Conquer/MatrixMultiplication.cpp
@@ -1,45 +1,85 @@
-//Matrix multiplication using divide & conquer
 #include <bits/stdc++.h>
 using namespace std;
-
 void add(int A[10][10], int B[10][10], int C[10][10], int n){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            C[i][j] = A[i][j] + B[i][j];
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            C[i][j]=A[i][j]+ B[i][j];
         }
     }
 }
-
-void MM(int A[10][10], int B[10][10], int C[10][10], int n){
-    if(n==1){
-        C[0][0] = A[0][0] * B[0][0];
+void multiply (int A[10][10], int B[10][10], int C[10][10], int n){
+    if (n==1){
+        C[0][0]=A[0][0]*B[0][0];
     }
-    int k = n/2;
-    //Create temporary matrices
+    int k=n/2;
     int A11[10][10], A12[10][10], A21[10][10], A22[10][10];
     int B11[10][10], B12[10][10], B21[10][10], B22[10][10];
-    int C11[10][10] ={0}, C12[10][10]={0}, C21[10][10]={0}, C22[10][10]={0};
-
-    int M1[10][10] ={0}, M2[10][10]={0};
-
-    for(int i=0; i<k; i++){
-        for(int j=0; j<k; j++){
-            A11[i][j] = A[i][j];
-            A12[i][j] = A[i][j+k];
-            A21[i][j] = A[i+k][j];
-            A22[i][j] = A[i+k][j+k];
-
-            B11[i][j] = B[i][j];
-            B12[i][j] = B[i][j+k];
-            B21[i][j] = B[i+k][j];
-            B22[i][j] = B[i+k][j+k];
+    int C11[10][10]={0}, C12[10][10]={0}, C21[10][10]={0}, C22[10][10]={0};
+    int M1[10][10]={0}, M2[10][10]={0}, M3[10][10]={0}, M4[10][10]={0};
+    for (int i=0;i<k;i++){
+        for (int j=0;j<k;j++){
+            A11[i][j]=A[i][j];
+            A12[i][j]=A[i][j+k];
+            A21[i][j]=A[i+k][j];
+            A22[i][j]=A[i+k][j+k];
+            B11[i][j]=B[i][j];
+            B12[i][j]=B[i][j+k];
+            B21[i][j]=B[i+k][j];
+            B22[i][j]=B[i+k][j+k];
         }
     }
-    MM(A11, B11, M1, k); //A11*B11
-    MM(A12, B21, M2, k); //A12*B21
-    add(M1, M2, C11, k); //C11 = M1 + M2
+    multiply(A11, B11, M1, k);
+    multiply(A12, B21, M2, k);
+    add(M1, M2, C11, k);
 
-    MM(A11, B12, M1, k); //A11*B12
-    MM(A12, B22, M2, k); //A12*B22  
-    add(M1, M2, C12, k); //C12 = M1 + M2
+    multiply(A11, B12, M1, k);
+    multiply(A12, B22, M2, k);
+    add(M1, M2, C12, k);
+    multiply(A21, B11, M1, k);
+    multiply(A22, B21, M2, k);
+    add(M1, M2, C21, k);
+
+    multiply(A21, B12, M1, k);
+    multiply(A22, B22, M2, k);
+    add(M1, M2, C22, k);
+
+    for (int i=0;i<k;i++){
+        for (int j=0;j<k;j++){
+            C[i][j]=C11[i][j];
+            C[i][j+k]=C12[i][j];
+            C[i+k][j]=C21[i][j];
+            C[i+k][j+k]=C22[i][j];
+        }
+    }
+}
+void printmatrix(int M[10][10], int n){
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            cout<<M[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int A[10][10], B[10][10], C[10][10]={0};
+    int n, i, j;
+    cout<<"Enter order of matrix (n x n): ";
+    cin>>n;
+    cout<<"Enter elements of matrix A: "<<endl;
+    for (i=0;i<n;i++){
+        for (j=0;j<n;j++){
+            cin>>A[i][j];
+        }
+    }
+    cout<<"Enter elements of matrix B: "<<endl;
+    for (i=0;i<n;i++){
+        for (j=0;j<n;j++){
+            cin>>B[i][j];
+        }
+    }
+    multiply(A, B, C, n);
+    cout<<"Product of matrix A and B is: "<<endl;
+    printmatrix(C, n);
+    return 0;
 }
